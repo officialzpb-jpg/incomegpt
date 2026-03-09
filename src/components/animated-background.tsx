703221e6367e938e06252c08666852549a498fdd
officialzpb-jpg <officialzpb@gmail.com>
2026-03-09 02:53:03 -0400

    Fix TypeScript errors in animated background - pass canvas dimensions to Particle class

--- src/components/animated-background.tsx
+++ src/components/animated-background.tsx
@@ -28,10 +28,14 @@ export function AnimatedBackground() {
       speedY: number;
       opacity: number;
       color: string;
+      canvasWidth: number;
+      canvasHeight: number;
 
-      constructor() {
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
+      constructor(canvasW: number, canvasH: number) {
+        this.canvasWidth = canvasW;
+        this.canvasHeight = canvasH;
+        this.x = Math.random() * this.canvasWidth;
+        this.y = Math.random() * this.canvasHeight;
         this.size = Math.random() * 2 + 0.5;
         this.speedX = (Math.random() - 0.5) * 0.5;
         this.speedY = (Math.random() - 0.5) * 0.5;
@@ -47,10 +51,10 @@ export function AnimatedBackground() {
         this.y += this.speedY;
 
         // Wrap around screen
-        if (this.x > canvas.width) this.x = 0;
-        if (this.x < 0) this.x = canvas.width;
-        if (this.y > canvas.height) this.y = 0;
-        if (this.y < 0) this.y = canvas.height;
+        if (this.x > this.canvasWidth) this.x = 0;
+        if (this.x < 0) this.x = this.canvasWidth;
+        if (this.y > this.canvasHeight) this.y = 0;
+        if (this.y < 0) this.y = this.canvasHeight;
       }
 
       draw() {
@@ -68,7 +72,7 @@ export function AnimatedBackground() {
       particles = [];
       const particleCount = Math.min(Math.floor(window.innerWidth / 10), 100);
       for (let i = 0; i < particleCount; i++) {
-        particles.push(new Particle());
+        particles.push(new Particle(canvas.width, canvas.height));
       }
     };
 
